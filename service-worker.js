d4f6b20722a98425a0f87363a704cba2a37e2bce
Negaex <gh@negaex.com>
2019-04-12 13:04:23 +1000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/renxcs-ts/precache-manifest.1f733e86935058e3b5c845229594200c.js"
+  "/renxcs-ts/precache-manifest.fe260c86c9d7ef068654b8ce6a3590e8.js"
 );
 
 workbox.clientsClaim();
