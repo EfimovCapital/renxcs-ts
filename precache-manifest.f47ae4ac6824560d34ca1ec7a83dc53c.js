d070d43669b54d69db89439ab80c553a07091c9a
Negaex <gh@negaex.com>
2019-04-12 16:07:31 +1000

    Updates

---
+++ precache-manifest.f47ae4ac6824560d34ca1ec7a83dc53c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "e021baf7b8612299f89970a50a1ddb4f",
+    "url": "/renxcs-ts/static/media/rp-flag-uk.e021baf7.svg"
+  },
+  {
+    "revision": "035b2edd71fad2524716f7e665bad58b",
+    "url": "/renxcs-ts/static/media/metamask.035b2edd.svg"
+  },
+  {
+    "revision": "3e2049306f4f9842ce12eb02bfa9f66e",
+    "url": "/renxcs-ts/static/media/logo.3e204930.svg"
+  },
+  {
+    "revision": "5e9b8ce67fb7d433b519",
+    "url": "/renxcs-ts/static/js/runtime~main.9e94aeb8.js"
+  },
+  {
+    "revision": "151ea870437e9d5fb630",
+    "url": "/renxcs-ts/static/js/main.3543da0e.chunk.js"
+  },
+  {
+    "revision": "0fc76f5bbd3956fe3719",
+    "url": "/renxcs-ts/static/js/2.061bd237.chunk.js"
+  },
+  {
+    "revision": "151ea870437e9d5fb630",
+    "url": "/renxcs-ts/static/css/main.9d53caf5.chunk.css"
+  },
+  {
+    "revision": "0fc76f5bbd3956fe3719",
+    "url": "/renxcs-ts/static/css/2.b6713031.chunk.css"
+  },
+  {
+    "revision": "92fb9058ca55f3eceec5099263abc8f8",
+    "url": "/renxcs-ts/index.html"
+  }
+];
